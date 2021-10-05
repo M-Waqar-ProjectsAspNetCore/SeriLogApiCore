24ffc6cb0df4c57b1c344c39c488ad24bc0265fc
Muhammad Waqar <Muhammad.waqar@tiybr.com>
2021-10-05 15:06:17 +0400

    Add project files.

---
+++ SeriLogApiCore/Controllers/WeatherForecastController.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SeriLogApiCore.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        private readonly ILogger<WeatherForecastController> _logger;
+
+        public WeatherForecastController(ILogger<WeatherForecastController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IEnumerable<WeatherForecast> Get()
+        {
+            try
+            {
+                _logger.LogInformation("Log From Weather Forecast Get Method");
+                // Can not divide by zero (Exception)
+                int a = 5;
+                int b = 0;
+                int x = a / b;
+
+                var rng = new Random();
+                return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+                {
+                    Date = DateTime.Now.AddDays(index),
+                    TemperatureC = rng.Next(-20, 55),
+                    Summary = Summaries[rng.Next(Summaries.Length)]
+                })
+                .ToArray();
+            }
+            catch (Exception ex)
+            {
+                //_logger.LogError(ex.Message);
+                throw;
+            }
+        }
+    }
+}
---
+++ SeriLogApiCore/Program.cs
@@ -0,0 +1,47 @@
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Serilog;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SeriLogApiCore
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            try
+            {
+                var configuration = new ConfigurationBuilder()
+                    .AddJsonFile("appsettings.json")
+                    .Build();
+
+                Log.Logger = new LoggerConfiguration()
+                    .ReadFrom.Configuration(configuration)
+                    .CreateLogger();
+
+                //BASIC WITHOUT ANY CONFIGURATION CONTAINS ALL LOGS
+                //BASIC Will work with the appsetting default Microsoft logging settings. 
+                //Log.Logger = new LoggerConfiguration()
+                //    .WriteTo.File("Logs/log.txt", rollingInterval: RollingInterval.Day).CreateLogger();
+                CreateHostBuilder(args).Build().Run();
+            }
+            finally
+            {
+                Log.CloseAndFlush();
+            }
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+            .UseSerilog()
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseStartup<Startup>();
+                });
+    }
+}
